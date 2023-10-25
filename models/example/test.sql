fed0f2edc61177c43ddc1a72dad0511a66881bbc
DavidL <davit.loria@restorepoint.ai>
2023-10-25 15:18:37 +0400

    initial

--- models/example/test.sql
+++ models/example/test.sql
@@ -1,6 +1,6 @@
 WITH transformed_data AS (
   SELECT
-    date AS observation_date,
+    "date" AS observation_date,
     new_recovered AS recovered,
     new_tested AS tested,
     total_deceased AS deceased,
@@ -9,11 +9,11 @@ WITH transformed_data AS (
     total_confirmed AS total_cases,
     total_tested AS total_tests,
     total_recovered AS total_recoveries,
-    key,
-    _airbyte_ab_id AS airbyte_id,
-    _airbyte_emitted_at AS emitted_at,
-    _airbyte_normalized_at AS normalized_at,
-    _airbyte_covid_data_hashid AS data_hashid
+    "key",
+    "_airbyte_ab_id" AS airbyte_id,
+    "_airbyte_emitted_at" AS emitted_at,
+    "_airbyte_normalized_at" AS normalized_at,
+    "_airbyte_covid_data_hashid" AS data_hashid
   FROM your_project_name.covid_data
 )
 
